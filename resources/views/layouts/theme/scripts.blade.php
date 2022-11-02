b290e191948f9dca7da4a0e325a5bbf132914fd1
Jorge Castañeda <jcastaneda@tamti.com.mx>
2022-11-02 10:57:35 -0600

    update scripts

--- resources/views/layouts/theme/scripts.blade.php
+++ resources/views/layouts/theme/scripts.blade.php
@@ -10,9 +10,8 @@
         });
     </script>
     <script src="{{ asset('assets/js/custom.js') }}"></script>
+    <script src="{{ asset('plugins/sweetalerts/sweetalert2.min.js') }}"></script>
+    <script src="{{ asset('plugins/notification/snackbar/snackbar.min.js') }}"></script>
+    <script src="{{ asset('plugins/nicescroll/nicescroll.js') }}"></script>
+    <script src="{{ asset('plugins/currency/currency.js') }}"></script>
     <!-- END GLOBAL MANDATORY SCRIPTS -->
-
-    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
-    <script src="{{ asset('plugins/apex/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('assets/js/dashboard/dash_2.js') }}"></script>
-    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->

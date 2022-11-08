585c45692bcbf8a2cc0bc7dcf69e3788d5d0be64
Jorge Castañeda <jcastaneda@tamti.com.mx>
2022-11-08 02:30:18 -0600

    create category livewire component

---
+++ app/Http/Livewire/Categories.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Category;
+use Livewire\WithPagination;
+use Livewire\WithFileUploads;
+
+class Categories extends Component
+{
+    use WithFileUploads;
+    use WithPagination;
+
+    public $name, $search, $image, $selected_id, $pageTitle, $componentName;
+    private $pagination = 5;
+
+
+    public function render()
+    {
+        $data = Category::all();
+
+        return view('livewire.category.categories', ['data' => $data])
+        ->extends('layouts.theme.app')
+        ->section('content');
+    }
+}

6366f7d9825ac508f1f1f5d09bacb3512fd8bf48
Jorge Castañeda <jcastaneda@tamti.com.mx>
2022-11-08 01:54:13 -0600

    create component template

---
+++ resources/views/livewire/startpage/init.php
@@ -0,0 +1,56 @@
+<div class="row sales layout-top-spacing">
+    <div class="col-sm-12">
+        <div class="widget widget-chart-one">
+            <div class="gidget-heading">
+                <h4 class="card-title">
+                    <b>ComponentName | PageTitle</b>
+                </h4>
+                <ul class="tabs tabs-pills">
+                    <li>
+                        <a href="javascript:void(0)" class="tabmenu -bg-dark" data-toggle="modal" data-target="#theModal">Agregar</a>
+                    </li>
+                </ul>
+            </div>
+            Search
+
+            <div class="widget-content">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped mt-1">
+                        <thead class="text-white" style="background: #3B3F5C">
+                            <tr>
+                                <th class="table-th text-white">Descripción</th>
+                                <th class="table-th text-white">Imagen</th>
+                                <th class="table-th text-white">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><h6>Category name</h6></td>
+                                <td class="text-center">
+                                    <span>
+                                        <img src="" alt="imagen de ejemplo" height="70" width="80" class="rounded">
+                                    </span>
+                                </td>
+                                <td class="text-center">
+                                    <a href="javascript:void(0)" class="btn btn-dark mtmobile" title="Edit">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <a href="javascript:void(0)" class="btn btn-dark" title="Delete">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    Pagination
+                </div>
+            </div>
+        </div>
+    </div>
+    Include form
+</div>
+<script>
+    document.addEventLister('DOMContentLoaded', function(){
+
+    });
+</script>

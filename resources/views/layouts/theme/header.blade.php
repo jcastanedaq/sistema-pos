5ecd9e4a1ee431c729e39122973b50e6a1873c3f
Jorge Castañeda <jcastaneda@tamti.com.mx>
2022-11-08 01:38:43 -0600

    update header blade

--- resources/views/layouts/theme/header.blade.php
+++ resources/views/layouts/theme/header.blade.php
@@ -3,7 +3,8 @@
             <ul class="navbar-item flex-row">
                 <li class="nav-item theme-logo">
                     <a href="index.html">
-                        <img src="assets/img/90x90.jpg" class="navbar-logo" alt="logo">
+                        <img src="{{ asset('assets/img/livewire-logo.png') }}" class="navbar-logo" alt="logo">
+                        <b style="font-size: 19px; color:#3B3F5C">LWPOS</b>
                     </a>
                 </li>
             </ul>
@@ -23,12 +24,12 @@
             <ul class="navbar-item flex-row navbar-dropdown">
                 <li class="nav-item dropdown user-profile-dropdown  order-lg-0 order-1">
                     <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="assets/img/90x90.jpg" alt="admin-profile" class="img-fluid">
+                        <i class="far fa-user text-dark"></i>
                     </a>
                     <div class="dropdown-menu position-absolute animated fadeInUp" aria-labelledby="userProfileDropdown">
                         <div class="user-profile-section">
                             <div class="media mx-auto">
-                                <img src="assets/img/90x90.jpg" class="img-fluid mr-2" alt="avatar">
+                                <img src="{{ asset('assets/img/laravel-logo.jpg') }}" class="img-fluid mr-2" alt="avatar">
                                 <div class="media-body">
                                     <h5>Jorge Castañeda</h5>
                                     <p>Developer</p>
@@ -37,7 +38,8 @@
                         </div>
                         <div class="dropdown-item">
                             <a href="user_profile.html">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> <span>Mi Perfil</span>
+                                <i class="far fa-user"></i>
+                                <span>Mi Perfil</span>
                             </a>
                         </div>
                         <div class="dropdown-item">

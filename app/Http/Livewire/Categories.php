31c1aaa14ac35bdaa6935cb5c11ce96b60cd03fd
Jorge Castañeda <webmaster@tamti.com.mx>
2023-03-08 02:44:38 -0600

    add searchbox function to controller

--- app/Http/Livewire/Categories.php
+++ app/Http/Livewire/Categories.php
@@ -25,7 +25,12 @@ class Categories extends Component
 
     public function render()
     {
-        $data = Category::all();
+        if(strlen($this->search) > 0)
+        {
+            $data = Category::where('name', 'like', '%'.$this->search.'%')->paginate($this->pagination);
+        }else{
+            $data = Category::orderBy('id', 'desc')->paginate($this->pagination);
+        }
 
         return view('livewire.category.categories', ['data' => $data,])
         ->extends('layouts.theme.app')

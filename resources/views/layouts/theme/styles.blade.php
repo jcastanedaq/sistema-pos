971c7c931c9aaef6d3d1c819b7aba6d2fc57bebc
Jorge Castañeda <jcastaneda@tamti.com.mx>
2023-06-27 01:46:03 -0600

    update style css of sidebar

--- resources/views/layouts/theme/styles.blade.php
+++ resources/views/layouts/theme/styles.blade.php
@@ -49,6 +49,25 @@
             display: block;
         }
     }
+
+    .sidebar-theme #compactSidebar {
+        background: #191e3a !important;
+    }
+
+    .header-container .sidebarCollapse {
+        color: #3b3f5c !important;
+    }
+
+    .navbar .navbar-item .nav-item form.form-inline input.search-form-control {
+        font-size: 15px;
+        background-color: #3b3f5c !important;
+        padding-right: 40px;
+        padding-top: 12px;
+        border: none;
+        color: #fff;
+        box-shadow: none;
+        border-radius: 30px;
+    }
 </style>
 
 @livewireStyles

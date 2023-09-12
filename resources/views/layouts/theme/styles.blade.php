2b576d96d4642f0d714d38ca05dfe4b99f1c5b29
Jorge Castañeda <jcastaneda@tamti.com.mx>
2023-09-12 08:57:21 -0600

    update styles blade

--- resources/views/layouts/theme/styles.blade.php
+++ resources/views/layouts/theme/styles.blade.php
@@ -20,6 +20,9 @@
 
 <link href="{{ asset('assets/css/widgets/modules-widgets.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/css/forms/theme-checkbox-radio.css') }}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('assets/css/apps/scrumboard.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/apps/notes.css') }}" rel="stylesheet" type="text/css" />
 <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
 
 <style>

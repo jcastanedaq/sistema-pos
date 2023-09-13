d6d412f1133251a3295af37ec010c506e7b3dd7d
Jorge Castañeda <jcastaneda@tamti.com.mx>
2023-09-12 22:03:05 -0600

    make pos component

--- app/Http/Livewire/Pos.php
+++ app/Http/Livewire/Pos.php
@@ -3,12 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Denomination;
 
 class Pos extends Component
 {
-    public $total, $cart, $itemsQuantity = 0;
+    public $total = 800, $cart = [], $itemsQuantity = 0, $denominations = [], $efectivo = 10000, $change = 200;
     public function render()
     {
+        $this->denominations = Denomination::all();
         return view('livewire.pos.component')
         ->extends('layouts.theme.app')->section('content');
     }
